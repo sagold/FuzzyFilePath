468df8c44a871edf0665550f077e34af0f6cbae5
Sascha Goldhofer <post@saschagoldhofer.de>
2014-10-26 14:20:35 +0100

    Update verbose prefixes

--- common/verbose.py
+++ common/verbose.py
@@ -1,4 +1,7 @@
-DEBUG = True
+from FuzzyFilePath.common.config import config
+
+IGNORE = ["view", "query"]
+
 def verbose(*args):
-    if DEBUG is True:
+    if config["DEBUG"] is True and not args[0] in IGNORE:
         print("FFP\t", *args)
