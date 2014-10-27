3b80b19975d2ff877cdb3245beb3c1d0eae1cd68
Sascha Goldhofer <post@saschagoldhofer.de>
2014-10-27 21:45:14 +0100

    Update verbose flags

--- common/verbose.py
+++ common/verbose.py
@@ -1,6 +1,6 @@
 from FuzzyFilePath.common.config import config
 
-IGNORE = ["view", "query"]
+IGNORE = ["view", "query", "cache"]
 
 def verbose(*args):
     if config["DEBUG"] is True and not args[0] in IGNORE:
