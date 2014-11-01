5bc919c36c19da53a1b201635f0af85928466236
Sascha Goldhofer <post@saschagoldhofer.de>
2014-11-01 15:23:09 +0100

    Update config

--- common/config.py
+++ common/config.py
@@ -2,5 +2,10 @@ config = {
 
 	"DEBUG": False,
 	"FFP_SETTINGS_FILE": "FuzzyFilePath.sublime-settings",
-	"ESCAPE_DOLLAR": '\$'
+	"ESCAPE_DOLLAR": '\$',
+	"TRIGGER_ACTION": ["auto_complete", "insert_path"],
+	"INSERT_ACTION": ["commit_completion", "insert_best_completion"],
+	# dynamic
+	"DISABLE_AUTOCOMPLETION": False,
+	"DISABLE_KEYMAP_ACTIONS": False
 }
