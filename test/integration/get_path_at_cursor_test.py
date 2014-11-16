93db8c8e4e58453bcb8d3d19d7aed87fa5e673d0
Sascha Goldhofer <post@saschagoldhofer.de>
2014-11-16 20:14:52 +0100

    Update integration tests

--- test/integration/get_path_at_cursor_test.py
+++ test/integration/get_path_at_cursor_test.py
@@ -4,10 +4,34 @@ from FuzzyFilePath.context import get_path_at_cursor
 
 class Test(TestCase):
 
-	def should_return_path(self, viewHelper):
+	def should_return_complete_line(self, viewHelper):
+		viewHelper.set_line('../path/in/quotes')
+		viewHelper.move_cursor(0, 12)
+
+		path = get_path_at_cursor(viewHelper.view)[0]
+
+		assert path == "../path/in/quotes", "expected '%s' to be '../path/in/quotes'" % path
+
+	def should_return_path_in_quotes(self, viewHelper):
 		viewHelper.set_line('in "./path/in/quotes" another in')
 		viewHelper.move_cursor(0, 12)
 
 		path = get_path_at_cursor(viewHelper.view)[0]
 
 		assert path == "./path/in/quotes", "expected '%s' to be './path/in/quotes'" % path
+
+	def should_return_correct_needle(self, viewHelper):
+		viewHelper.set_line('needle /needle needle')
+		viewHelper.move_cursor(0, 12)
+
+		path = get_path_at_cursor(viewHelper.view)[0]
+
+		assert path == "/needle", "expected '%s' to be '/needle'" % path
+
+	def should_return_empty_string(self, viewHelper):
+		viewHelper.set_line('""')
+		viewHelper.move_cursor(0, 1)
+
+		path = get_path_at_cursor(viewHelper.view)[0]
+
+		assert path == "", "expected '%s' to be empty ''" % path
