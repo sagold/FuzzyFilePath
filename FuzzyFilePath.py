3a470753cb44625adcd926618e7fc8db1e1a328b
Sascha Goldhofer <post@saschagoldhofer.de>
2014-11-19 22:14:07 +0100

    Refactor move event query actions to functions

--- FuzzyFilePath.py
+++ FuzzyFilePath.py
@@ -1,22 +1,9 @@
 """ FuzzyFilePath
     Manages filepath autocompletions
 
-    # possible tasks
+    # tasks
 
-        - option to trigger by current line context, i.e. url() or <img ... src="">
-        - alternatively use triggers like "graffin:" instead/additionally to scope-triggers
         - support multiple folders
-        - Cursor Position after replacement:
-            require("../../../../optimizer|cursor|")
-            SHOULD BE:
-            require("../../../../optimizer")|cursor|
-
-    # bugs
-
-        - uncleaned completion if triggered in middle of word (~> Completion before)
-        - long idle time
-            - for multi cursor in paths (~> do not query anything!)
-            - ???
 
     # errors
 
@@ -38,6 +25,13 @@ from FuzzyFilePath.common.config import config
 query = Query()
 project_files = None
 
+
+
+def get_current_line(view):
+    selection = view.sel()[0]
+    position = selection.begin()
+    line_region = view.line(position)
+    return view.substr(line_region)
 
 def get_path_at_cursor(view):
     """
@@ -77,14 +71,14 @@ class Completion:
 
 
 def plugin_loaded():
-    """load settings"""
+    """ load settings """
     settings = sublime.load_settings(config["FFP_SETTINGS_FILE"])
     settings.add_on_change("extensionsToSuggest", update_settings)
     update_settings()
 
 
 def update_settings():
-    """restart projectFiles with new plugin and project settings"""
+    """ restart projectFiles with new plugin and project settings """
     global project_files, config
 
     exclude_folders = []
@@ -100,10 +94,68 @@ def update_settings():
     config["DEBUG"] = settings.get("DEBUG", config["DEBUG"])
 
 
+
+def cleanup_completion(view):
+    path = get_path_at_cursor(view)
+    # remove path query completely
+    final_path = Completion.get_final_path(path[0])
+    verbose("insert", "final path", final_path)
+    # replace current query with final path
+    view.run_command("ffp_replace_region", { "a": path[1].a, "b": path[1].b, "string": final_path })
+    Completion.reset()
+
+
+
+def query_completions(view, prefix, locations):
+    if query.valid is False:
+        return False
+
+    # needle = context.get_path_at_cursor(view)[0]
+    needle = get_path_at_cursor(view)[0]
+    current_scope = view.scope_name(locations[0])
+
+    if query.build(current_scope, needle, query.relative) is False:
+        return None
+
+    print("FFP QUERYING FILES")
+    completions = project_files.search_completions(query.needle, query.project_folder, query.extensions, query.relative, query.extension)
+    print("FFP QUERYING FILES DONE")
+
+    if len(completions[0]) > 0:
+        verbose("completions", len(completions[0]), "matches found for", query.needle)
+        Completion.active = True
+        Completion.replaceOnInsert = query.replace_on_insert
+        # vintageous
+        view.run_command('_enter_insert_mode')
+    else:
+        verbose("completions", "no completions for", query.needle)
+        Completion.active = False
+
+    query.reset()
+    return completions
+
+
+
+def update_project_files(view):
+    if project_files is not None:
+        for folder in sublime.active_window().folders():
+            if folder in view.file_name():
+                project_files.update(folder, view.file_name())
+
+def update_project_folders(view):
+    file_name = view.file_name()
+    folders = sublime.active_window().folders()
+
+    if (project_files is None):
+        query.valid = False
+        return False
+
+    if query.update(folders, file_name):
+        project_files.add(query.project_folder)
+
+
 class InsertPathCommand(sublime_plugin.TextCommand):
-    """
-        trigger customized autocomplete
-    """
+    """ trigger customized autocomplete """
     def run(self, edit, type="default", replace_on_insert=[]):
         if config["DISABLE_KEYMAP_ACTIONS"] is True:
             return False
@@ -117,37 +169,60 @@ class InsertPathCommand(sublime_plugin.TextCommand):
 
 
 class FuzzyFilePath(sublime_plugin.EventListener):
-    """
-        track and validate custom event: on_post_insert_completion
-    """
+
     track_insert = {
         "active": False,
         "start_line": "",
-        "start_path": "",
         "end_line": "",
         "end_path": ""
     }
 
+    def on_query_completions(self, view, prefix, locations):
+        """ query filepath completion """
+        # check if a completion may be inserted
+        if self.track_insert["active"] is False:
+            self.start_tracking(view)
+
+        if (config["DISABLE_AUTOCOMPLETION"] is True):
+            return None
+
+        return query_completions(view, prefix, locations)
+
+
+    def on_post_insert_completion(self, view, command_name):
+        """ post filepath completion """
+        if Completion.active is True:
+            Completion.active = False
+            cleanup_completion(view)
+
+
+    def on_post_save_async(self, view):
+        update_project_files(view)
+
+
+    def on_activated(self, view):
+        update_project_folders(view)
+
+
     def start_tracking(self, view, command_name=None):
         self.track_insert["active"] = True
-        self.track_insert["start_line"] = context.get_line_at_cursor(view)[0]
+        self.track_insert["start_line"] = get_current_line(view)
         self.track_insert["end_line"] = None
-        self.track_insert["start_path"] = get_path_at_cursor(view)
-        verbose("--> trigger", command_name, self.track_insert["start_line"], self.track_insert["start_path"])
+        # verbose("--> trigger", command_name, self.track_insert["start_line"])
+        word_replaced = re.split("[./]", self.track_insert["start_line"]).pop()
+        if (self.track_insert["start_line"] is not word_replaced):
+            Completion.before = re.sub(re.escape(word_replaced) + "$", "", self.track_insert["start_line"])
 
-        path = get_path_at_cursor(view)
-        word_replaced = re.split("[./]", path[0]).pop()
-        if (path is not word_replaced):
-            Completion.before = re.sub(re.escape(word_replaced) + "$", "", path[0])
 
     def finish_tracking(self, view, command_name=None):
         self.track_insert["active"] = False
-        self.track_insert["end_line"] = context.get_line_at_cursor(view)[0]
-        self.track_insert["end_path"] = get_path_at_cursor(view)
-        verbose("<-- insert", command_name, self.track_insert["end_path"])
+        self.track_insert["end_line"] = get_current_line(view)
+        # verbose("<-- insert", command_name)
+
 
     def abort_tracking(self):
         self.track_insert["active"] = False
+
 
     def on_text_command(self, view, command_name, args):
         # check if a completion may be inserted
@@ -157,91 +232,11 @@ class FuzzyFilePath(sublime_plugin.EventListener):
             Completion.active = False
             self.abort_tracking()
 
+
     def on_post_text_command(self, view, command_name, args):
         # check if a completion is inserted
-        current_line = context.get_line_at_cursor(view)[0]
-        insert = command_name in config["TRIGGER_ACTION"] and self.track_insert["start_line"] != current_line
-        insert = insert or command_name in config["INSERT_ACTION"]
-
-        if insert is True:
+        current_line = get_current_line(view) #context.get_line_at_cursor(view)[0]
+        command_trigger = command_name in config["TRIGGER_ACTION"] and self.track_insert["start_line"] != current_line
+        if command_trigger or command_name in config["INSERT_ACTION"]:
             self.finish_tracking(view, command_name)
             self.on_post_insert_completion(view, command_name)
-
-    """
-        query filepath completion
-    """
-    def on_query_completions(self, view, prefix, locations):
-        # check if a completion may be inserted
-        if self.track_insert["active"] is False:
-            self.start_tracking(view)
-
-        if (config["DISABLE_AUTOCOMPLETION"] is True):
-            return None
-
-        if query.valid is False:
-            return False
-
-        # needle = context.get_path_at_cursor(view)[0]
-        needle = get_path_at_cursor(view)[0]
-        current_scope = view.scope_name(locations[0])
-
-        if query.build(current_scope, needle, query.relative) is False:
-            return None
-
-        print("FFP QUERYING FILES")
-        completions = project_files.search_completions(query.needle, query.project_folder, query.extensions, query.relative, query.extension)
-        print("FFP QUERYING FILES DONE")
-
-        if len(completions[0]) > 0:
-            verbose("completions", len(completions[0]), "matches found for", query.needle)
-            Completion.active = True
-            Completion.replaceOnInsert = query.replace_on_insert
-            # vintageous
-            view.run_command('_enter_insert_mode')
-        else:
-            verbose("completions", "no completions for", query.needle)
-            Completion.active = False
-
-        query.reset()
-        return completions
-
-    """
-        post filepath completion
-    """
-    def on_post_insert_completion(self, view, command_name):
-        """ Sanitize inserted path by
-            - replacing temporary variables (~$)
-            - replacing query partials, like "../<inserted path>"
-        """
-        if Completion.active is False:
-            return None
-
-        Completion.active = False
-        path = get_path_at_cursor(view)
-        # remove path query completely
-        final_path = Completion.get_final_path(path[0])
-        verbose("insert", "final path", final_path)
-        # replace current query with final path
-        view.run_command("ffp_replace_region", { "a": path[1].a, "b": path[1].b, "string": final_path })
-
-        Completion.reset()
-
-    """
-        update cached files
-    """
-    def on_post_save_async(self, view):
-        if project_files is not None:
-            for folder in sublime.active_window().folders():
-                if folder in view.file_name():
-                    project_files.update(folder, view.file_name())
-
-    def on_activated(self, view):
-        file_name = view.file_name()
-        folders = sublime.active_window().folders()
-
-        if (project_files is None):
-            query.valid = False
-            return False
-
-        if query.update(folders, file_name):
-            project_files.add(query.project_folder)
