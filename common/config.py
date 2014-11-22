80f2310e73c2a47e5827a92a6208f6ee842dcc88
Sascha Goldhofer <post@saschagoldhofer.de>
2014-11-22 20:06:26 +0100

    Add option base_directory to settings

--- common/config.py
+++ common/config.py
@@ -6,7 +6,8 @@ config = {
 	"TRIGGER_ACTION": ["auto_complete", "insert_path"],
 	"INSERT_ACTION": ["commit_completion", "insert_best_completion"],
 	"TRIGGER_STATEMENTS": ["prefix", "tagName", "style"],
-	# dynamic
+
+	"BASE_DIRECTORY": "",
 	"DISABLE_AUTOCOMPLETION": False,
 	"DISABLE_KEYMAP_ACTIONS": False,
 	"AUTO_TRIGGER": True,
