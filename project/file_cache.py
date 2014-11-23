48a52b9720b4526c5fe3f7681a51fb40b7e91996
Sascha Goldhofer <post@saschagoldhofer.de>
2014-11-23 01:13:03 +0100

    Fix filepaths in cache

--- project/file_cache.py
+++ project/file_cache.py
@@ -51,7 +51,7 @@ class FileCache(threading.Thread):
                 filename, extension = os.path.splitext(relative_path)
                 extension = extension[1:]
 
-                relative_path = re.sub("\$", config["ESCAPE_DOLLAR"], posix(relative_path))
+                relative_path = re.sub("\$", config["ESCAPE_DOLLAR"], relative_path)
 
                 if extension in self.extensions:
                     # $ hack, reversed in post_commit_completion
