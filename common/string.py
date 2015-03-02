db059a023c988c8fcd579aa711c71f3389939095
Sascha Goldhofer <post@saschagoldhofer.de>
2015-03-02 23:36:47 +0100

    Refactor move string diff functions to separate module

---
+++ common/string.py
@@ -0,0 +1,39 @@
+def get_diff(first, second):
+    # get intersection at start
+    start = get_start_diff(first, second)
+
+    # remove intersection to prevent end diff duplicates
+    first = first[len(start):]
+    second = second[len(start):]
+    end = get_end_diff(first, second)
+
+    return {
+        "start": start,
+        "end": end
+    }
+
+def get_start_diff(first, second):
+    index = 0
+    result = ""
+    for c in first:
+        if c is second[index]:
+            index += 1
+            result += c
+        else:
+            break
+
+    return result
+
+def get_end_diff(first, second):
+    first = first[::-1]
+    second = second[::-1]
+    index = 0
+    result = ""
+    for c in first:
+        if c is second[index]:
+            index += 1
+            result = c + result
+        else:
+            break
+
+    return result
