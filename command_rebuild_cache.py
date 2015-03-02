149ea359dbc52eaa5796ee041060134cd8e04a74
Sascha Goldhofer <post@saschagoldhofer.de>
2015-03-02 23:40:22 +0100

    Refactor move command to separate file

---
+++ command_rebuild_cache.py
@@ -0,0 +1,7 @@
+import sublime_plugin
+from FuzzyFilePath.project.ProjectManager import ProjectManager
+
+class FfpUpdateCacheCommand(sublime_plugin.TextCommand):
+    """ force update project-files cache """
+    def run(self, edit):
+        ProjectManager.rebuild_filecache()
