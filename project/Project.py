b6ee17a25b62d0c6c5d84d55b0968b6f1fdbe910
Sascha Goldhofer <post@saschagoldhofer.de>
2015-03-04 08:53:45 +0100

    Fix project settings initialization

--- project/Project.py
+++ project/Project.py
@@ -6,20 +6,29 @@ class Project():
 	filecache = None
 
 	def __init__(self, window, project_settings, ffp_settings):
+		"""
+			Cache project files and manage project settings
+
+			Parameters
+			----
+			window : Sublime.Window			- active window associated with project
+			project_settings : Dictionary	- project settings (like project.settings.FuzzyFilePath)
+			ffp_settings : Dictionary		- config merge with base settings, specified by sublime-settings
+		"""
 		self.window = window
 		self.filecache = ProjectFiles()
 
+		# create final settings object, by merging project specific settings with base settings
 		self.settings = {}
 		for key in ffp_settings:
-			self.settings[key.upper()] = project_settings.get(key)
+			self.settings[key.upper()] = ffp_settings.get(key)
 
 		for key in project_settings:
-			print(key, key.upper())
 			self.settings[key.upper()] = project_settings.get(key)
 
+		# setup project cache
 		valid_file_extensions = get_valid_extensions(project_settings, ffp_settings)
 		folders_to_exclude = project_settings.get("EXCLUDE_FOLDERS", ffp_settings["EXCLUDE_FOLDERS"])
-		# setup project cache
 		self.filecache.update_settings(valid_file_extensions, folders_to_exclude)
 
 		# pay attention to multiple project folders
@@ -30,14 +39,16 @@ class Project():
 	def get_setting(self, key):
 		return self.settings.get(key)
 
+	def get_settings(self):
+		return self.settings
+
 	def set_setting(self, key, value):
 		data = self.window.get_project_data()
 		settings = data.get("settings").get("FuzzyFilePath")
 		settings[key] = value
 		self.window.set_project_data(data)
-
-	def get_settings(self):
-		return self.settings
+		# and update current settings
+		self.settings[key] = value
 
 	def cache_directory(self, directory):
 		return self.filecache.add(directory)
