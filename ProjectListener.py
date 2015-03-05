b41e4c469cd76d0e9ddd2331b3c28d69a63afff7
Sascha Goldhofer <post@saschagoldhofer.de>
2015-03-05 17:40:29 +0100

    Fix ProjectListener to check for project switch

--- ProjectListener.py
+++ ProjectListener.py
@@ -2,30 +2,37 @@ import sublime
 import sublime_plugin
 
 from FuzzyFilePath.project.ProjectManager import ProjectManager
+from FuzzyFilePath.project.ProjectManager import get_project_folder
 
+
+ID = "ProjectListener"
 
 class ProjectListener(sublime_plugin.EventListener):
     """ listens ons window changes, delegating events to project manager """
 
-    previous_view = None
+    previous_project = None
     previous_window = None
 
     def on_activated(self, view):
-        if self.previous_view is not view.id():
-            self.previous_view = view.id()
-        else:
-            self.on_window_activated(view)
+        project_folder = get_project_folder(view.window())
+
+        if self.previous_project is not project_folder:
+            if self.previous_project is not None:
+                self.on_project_activated(view)
+            self.previous_project = project_folder
 
         if self.previous_window is not sublime.active_window().id():
             self.previous_window = sublime.active_window().id()
-            self.on_window_changed(view)
+            self.on_window_activated(view)
 
     # project has been refocused
     def on_window_activated(self, view):
+        print(ID, "window activated -> update current project")
         ProjectManager.update_project(view.window())
 
     # another (possible) project has been opened/focused
-    def on_window_changed(self, view):
+    def on_project_activated(self, view):
+        print(ID, "project activated -> activate project")
         ProjectManager.activate_project(view.window())
 
     # after file has been saved
