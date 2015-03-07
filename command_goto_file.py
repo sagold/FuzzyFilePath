3cdbcd4e051216018be59d31ffaf880c761b0452
Sascha Goldhofer <post@saschagoldhofer.de>
2015-03-07 10:33:36 +0100

    Add support multiple results and relative paths

--- command_goto_file.py
+++ command_goto_file.py
@@ -1,5 +1,6 @@
 import os
 import sublime
+import re
 import sublime_plugin
 
 from FuzzyFilePath.expression import Context
@@ -10,34 +11,36 @@ from FuzzyFilePath.common.path import Path
 class FfpGotoFileCommand(sublime_plugin.TextCommand):
     """ go to file """
     def run(self, edit):
-    	file_path = self.view.file_name()
+        current_directory = os.path.dirname(self.view.file_name())
 
-    	context = Context.get_context(self.view)
-    	if context.get("valid") is False:
-    		return
+        context = Context.get_context(self.view)
+        if context.get("valid") is False:
+            return print("abort, no valid path given")
 
-    	path = context.get("needle")
-    	project = ProjectManager.get_current_project()
-    	is_relative = Path.is_relative(path)
-    	if is_relative:
-            path = Path.get_absolute_path(file_path, path)
-            print("relative path", path)
+        path = context.get("needle")
+        project = ProjectManager.get_current_project()
+        is_relative = Path.is_relative(path)
+        if is_relative:
+            path = Path.get_absolute_path(current_directory, path)
+            path = re.sub(project.get_directory(), "", path)
 
-    	if path and project:
-    		files = project.find_file(path)
-    		print("goto file", path, files)
+        path = re.sub("^[\\\\/\.]", "", path)
 
-    		if len(files) == 0:
-    			print("failed finding file", path)
-    		elif len(files) == 1:
-    			self.open(project.get_directory(), files[0])
-    		else:
-    			print("multiple files found")
+        if path and project:
+            files = project.find_file(path)
 
+            if len(files) == 0:
+                print("failed finding file", path)
+            elif len(files) == 1:
+                self.open_file(project.get_directory(), files[0])
+            else:
+                self.files = files
+                self.view.show_popup_menu(files, self.select_file)
 
-    def open(self, project_folder, filepath):
-    	print(project_folder, filepath)
-    	path = os.path.join(project_folder, filepath)
-    	sublime.active_window().open_file(path)
+    def select_file(self, index):
+        sublime.active_window().open_file(self.files[index])
 
+    def open_file(self, project_folder, filepath):
+        path = os.path.join(project_folder, filepath)
+        sublime.active_window().open_file(path)
 
