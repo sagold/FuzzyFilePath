9b097b3603009370da523461c263838de5691f46
Sascha Goldhofer <post@saschagoldhofer.de>
2015-03-07 10:46:23 +0100

    Cleanup

--- command_goto_file.py
+++ command_goto_file.py
@@ -6,7 +6,9 @@ import sublime_plugin
 from FuzzyFilePath.expression import Context
 from FuzzyFilePath.project.ProjectManager import ProjectManager
 from FuzzyFilePath.common.path import Path
+from FuzzyFilePath.common.verbose import log
 
+ID = "goto file"
 
 class FfpGotoFileCommand(sublime_plugin.TextCommand):
     """ go to file """
@@ -15,28 +17,31 @@ class FfpGotoFileCommand(sublime_plugin.TextCommand):
 
         context = Context.get_context(self.view)
         if context.get("valid") is False:
-            return print("abort, no valid path given")
+            return log(ID, "abort, no valid path given:", context.get("needle"))
 
         path = context.get("needle")
         project = ProjectManager.get_current_project()
+
+        if not (path and project):
+            return log(ID, "path or project invalid", path, project)
+
         is_relative = Path.is_relative(path)
         if is_relative:
             path = Path.get_absolute_path(current_directory, path)
             path = re.sub(project.get_directory(), "", path)
 
         path = re.sub("^[\\\\/\.]", "", path)
+        files = project.find_file(path)
 
-        if path and project:
-            files = project.find_file(path)
+        if len(files) == 0:
+            return log(ID, "failed finding file", path)
 
-            if len(files) == 0:
-                print("failed finding file", path)
-            elif len(files) == 1:
-                self.open_file(project.get_directory(), files[0])
-            else:
-                self.files = files
-                self.project_folder = project.get_directory()
-                self.view.show_popup_menu(files, self.select_file)
+        if len(files) == 1:
+            self.open_file(project.get_directory(), files[0])
+        else:
+            self.files = files
+            self.project_folder = project.get_directory()
+            self.view.show_popup_menu(files, self.select_file)
 
     def select_file(self, index):
         self.open_file(self.project_folder, self.files[index])
