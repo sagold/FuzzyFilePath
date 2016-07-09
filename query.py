ac18e9e977d1195d063b678b438066319d152504
Sascha Goldhofer <post@saschagoldhofer.de>
2016-07-09 16:28:38 +0200

    Refactor simplify query

--- query.py
+++ query.py
@@ -11,13 +11,11 @@ from FuzzyFilePath.common.config import config
 state = {
 
     "extensions": ["*"],
-    "base_path": False,
-    "replace_on_insert": []
+    "base_path": False
 }
 
 override = {
-
-    # documentation only, will be removed
+    # set by insert_path_command: overrideable properties for next query
     "filepath_type": False,
     "extensions": [],
     "base_directory": "",
@@ -61,83 +59,88 @@ def get_replacements():
 
 
 def build(needle, trigger, current_folder):
-    force_type = override.get("filepath_type", False)
+    """
+        updates state object for given trigger. The state object is then used to query the file cache:
+
+        @see completion
+        ProjectManager.search_completions(
+                query.get_needle(),
+                current_file.get_project_directory(),
+                query.get_extensions(),
+                query.get_base_path()
+            )
+    """
     triggered = by_command()
-    filepath_type = "relative"
     needle = Path.sanitize(needle)
     needle_is_absolute = Path.is_absolute(needle)
-    needle_is_relative = Path.is_relative(needle)
-    needle_is_path = needle_is_absolute or needle_is_relative
-    # abort if autocomplete is not available
+
+    needle_is_path = needle_is_absolute or Path.is_relative(needle)
     if not triggered and trigger.get("auto", False) is False and needle_is_path is False:
-        # verbose("FFP no autocomplete")
         return False
-    # test path to trigger auto-completion by needle
-    if not triggered and trigger["auto"] is False and config["AUTO_TRIGGER"] and needle_is_absolute:
-        force_type = "absolute"
-    # base_directory: override - trigger - False
-    base_directory = trigger.get("base_directory", False)
-    base_directory = override.get("base_directory", base_directory)
-    #
-    # set current directory by force, else by trigger:
-    #
-    # trigger       |
-    # --------------|--------------------
-    # False         | use current file's directory
-    # True          | use settings: base_directory
-    # String        | use string as base_directory
-    # change base folder to base directory
-    #
-    if base_directory is True:
+
+    valid = triggered or (config["AUTO_TRIGGER"] if needle_is_path else trigger.get("auto", config["AUTO_TRIGGER"]))
+    if valid is False:
+        return False
+
+    """ Set current directory by force, else by trigger:
+
+        TRIGGER
+        ----------------------------------------
+        False       use current file's directory
+        True        use settings: base_directory
+        String      use string as base_directory
+    """
+    base_path = resolve_value("base_directory", trigger, False)
+    if base_path is True:
         current_folder = config["BASE_DIRECTORY"]
-    elif base_directory:
-        current_folder = Path.sanitize_base_directory(base_directory)
+    elif base_path:
+        current_folder = Path.sanitize_base_directory(base_path)
+
     # notify completion to replace path
-    if base_directory and needle_is_absolute:
+    if base_path and needle_is_absolute:
         Completion.set_base_directory(current_folder)
-    #
-    # filepath_type
-    #
-    # needle    | trigger rel   | force     | RESULT
-    # ----------|---------------|-----------|---------
-    # ?         | relative      | False     | relative
-    # ?         | absolute      | False     | absolute
-    # absolute  | *             | False     | absolute
-    # relative  | *             | False     | relative
-    # *         | *             | relative  | relative
-    # *         | *             | absolute  | absolute
-    #
-    if force_type:
-        filepath_type = force_type
-    elif needle_is_absolute:
-        filepath_type = "absolute"
-    elif needle_is_relative:
-        filepath_type = "relative"
+
+    state["base_path"] = current_folder if resolve_path_type(needle, trigger) == "relative" else False
+    state["replace_on_insert"] = resolve_value("replace_on_insert", trigger, [])
+    state["extensions"] = resolve_value("extensions", trigger, ["*"])
+    state["needle"] = sanitize_needle(needle, current_folder)
+
+    return True
+
+
+def resolve_path_type(needle, trigger):
+    """ ^
+        |  Force
+        | --------
+        |  Needle
+        | --------
+        | Trigger?
+    """
+    type_of_path = False
+    # test if forced by command
+    if override.get("filepath_type"):
+        type_of_path = override.get("filepath_type")
+    # test path to trigger auto-completion by needle
+    elif not by_command() and trigger.get("auto") is False and config["AUTO_TRIGGER"] and Path.is_absolute(needle):
+        type_of_path = "absolute"
+    elif Path.is_absolute(needle):
+        type_of_path = "absolute"
+    elif Path.is_relative(needle):
+        type_of_path = "relative"
     elif trigger.get("relative") is True:
-        filepath_type = "relative"
+        type_of_path = "relative"
     elif trigger.get("relative") is False:
-        filepath_type = "absolute"
+        type_of_path = "absolute"
 
-    state["base_path"] = current_folder if filepath_type == "relative" else False
-
-    # replacements: override - trigger - None
-    state["replace_on_insert"] = trigger.get("replace_on_insert", [])
-    state["replace_on_insert"] = override.get("replace_on_insert", state.get("replace_on_insert"))
-    # extensions: override - trigger - "js"
-    extensions = trigger.get("extensions", ["*"])
-    extensions = override.get("extensions", extensions)
-    state["extensions"] = extensions
-    state["needle"] = build_needle_query(needle, current_folder)
-    # strip any starting dots or slashes
-    state["needle"] = re.sub("^[\.\/]*", "", state.get("needle"))
-
-    if triggered or (config["AUTO_TRIGGER"] if needle_is_path else trigger.get("auto", config["AUTO_TRIGGER"])):
-        return True
-
-    return False
+    return type_of_path
 
 
-def build_needle_query(needle, current_folder):
+def resolve_value(key, trigger, default):
+    settings = trigger.get(key, default)
+    return override.get(key, settings)
+
+
+def sanitize_needle(needle, current_folder):
     """
         sanitizes requested path and replaces a starting ./ with the current (local) folder
         returns final needle
@@ -154,5 +157,8 @@ def build_needle_query(needle, current_folder):
 
     if needle.startswith("./"):
         needle = current_folder + re.sub("\.\/", "", needle)
+
+    # strip any starting dots or slashes
+    needle = re.sub("^[\.\/]*", "", needle)
 
     return needle
