04db6aa1b2aa0fc6030dd6f7d6e4e5d3488cd302
Sascha Goldhofer <post@saschagoldhofer.de>
2016-07-09 11:39:14 +0200

    Refactor move command to separate file

---
+++ command_insert_path.py
@@ -0,0 +1,20 @@
+import sublime_plugin
+from FuzzyFilePath.common.config import config
+from FuzzyFilePath.query import Query
+
+class InsertPathCommand(sublime_plugin.TextCommand):
+    """ trigger customized autocomplete overriding auto settings """
+    def run(self, edit, type="default", base_directory=None, replace_on_insert=[], extensions=[]):
+        print("INSERT PATH COMMAND")
+        if config["DISABLE_KEYMAP_ACTIONS"] is True:
+            return False
+
+        Query.force("filepath_type", type)
+        Query.force("base_directory", base_directory)
+
+        if len(replace_on_insert) > 0:
+            Query.force("replace_on_insert", replace_on_insert)
+        if len(extensions) > 0:
+            Query.force("extensions", extensions)
+
+        self.view.run_command('auto_complete', "insert")
