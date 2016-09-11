c4f1665b428107334db28aba2f499382f3e684ba
Sascha Goldhofer <post@saschagoldhofer.de>
2016-09-11 17:55:17 +0200

    Fix broken goto command

--- command_goto_file.py
+++ command_goto_file.py
@@ -1,52 +1,54 @@
 import os
-import sublime
 import re
+import sublime
 import sublime_plugin
 import FuzzyFilePath.expression as Context
 import FuzzyFilePath.common.path as Path
 from FuzzyFilePath.common.verbose import log
 import FuzzyFilePath.common.selection as Selection
+import FuzzyFilePath.current_state as state
 
-ID = "goto file"
+
+ID = "GotoFile"
+
 
 class FfpGotoFileCommand(sublime_plugin.TextCommand):
-    """ go to file """
+    """ open file under cursor """
     def run(self, edit):
         current_directory = os.path.dirname(self.view.file_name())
-
         context = Context.get_context(self.view)
         if context.get("valid") is False:
             return log(ID, "abort, no valid path given:", context.get("needle"))
 
         path = context.get("needle")
-        project = state.get_project_directory()
+        project_folder = state.get_project_directory()
 
-        if not (path and project):
-            return log(ID, "path or project invalid", path, project)
+        if not (path and project_folder):
+            return log(ID, "path or project invalid", path, project_folder)
 
         is_relative = Path.is_relative(path)
         if is_relative:
             path = Path.get_absolute_path(current_directory, path)
-            path = re.sub(project.get_directory(), "", path)
+            path = re.sub(project_folder, "", path)
 
         path = re.sub("^[\\\\/\.]", "", path)
-        files = project.find_file(path)
+        files = state.find_file(path)
 
         if len(files) == 0:
             return log(ID, "failed finding file", path)
 
         if len(files) == 1:
-            self.open_file(project.get_directory(), files[0])
+            self.open_file(project_folder, files[0])
         else:
             # if javascript, search for index.js
             current_scope = self.view.scope_name(Selection.get_position(self.view))
             if re.search("\.js ", current_scope):
                 for file in files:
                     if "index.js" in file:
-                        return self.open_file(project.get_directory(), file)
+                        return self.open_file(project_folder, file)
 
             self.files = files
-            self.project_folder = project.get_directory()
+            self.project_folder = project_folder
             self.view.show_popup_menu(files, self.select_file)
 
     def select_file(self, index):
